1f872fc0ca9e3c94f569c29faa67278dd5230dbb
Hazem Saleh <hazems@apache.org>
2018-07-29 04:40:44 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { HubConnection } from '@aspnet/signalr';
+import * as signalR from '@aspnet/signalr';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'channelApp';
+  hubConnection: HubConnection | undefined;
+  channels: any;
+  shows: Array<any>;
+  channelID: Number;
+  showsNotification: string;
+
+  constructor(private zone: NgZone) {
+    this.shows = [];
+  }
+
+  ngOnInit(): void {
+
+    this.hubConnection = new signalR.HubConnectionBuilder()
+                        .withUrl('http://channel-demo-app.centralus.cloudapp.azure.com/channelHub')
+                        .configureLogging(signalR.LogLevel.Information)
+                        .build();
+
+    this.hubConnection.start().catch(err => console.error(err.toString()));
+
+    this.hubConnection.on('getChannels', (channels: any) => {
+        this.zone.run(() => {
+          console.log("Length of received channels = " + channels.length);
+          this.channels = channels;        
+        });
+    });
+
+  }
+
+  public getChannels(): void {
+    if (this.hubConnection) {
+        console.log("Getting channels ...");
+        this.hubConnection.invoke('GetChannels');
+    }
+  }
+
+  public getShows(_channelID): void {
+    if (this.hubConnection) {
+       console.log("Getting shows for channel: " + _channelID);
+       this.shows = [];
+       this.showsNotification = "";
+
+       this.hubConnection.stream("GetShows", _channelID)
+            .subscribe({
+                next: (show) => {
+                  console.log("Receiving a show: " + show.name);
+                  this.zone.run(() => {
+                    this.shows.push(show);
+                  });
+                },
+                complete: () => {
+                  this.zone.run(() => {
+                    console.log("Get shows operation completes");
+                    this.showsNotification = "Get shows operation completes";    
+                  });            
+                },
+                error: (err) => {
+                  this.zone.run(() => {
+                    console.log("An error happens in getting shows ...");
+                    this.showsNotification = "An error happens in getting shows ...";   
+                  });             
+                }
+            });
+    }
+  }  
 }
