40a18d00b3cb9dc18117b33153c6b43b0922d717
Hazem Saleh <hazems@apache.org>
2018-07-31 13:28:48 -0400

    Implementing app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import * as signalR from '@aspnet/signalr';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  readonly hubEndPoint : string = "http://channel-demo-app.centralus.cloudapp.azure.com/channelHub"
   hubConnection: HubConnection | undefined;
   channels: any;
   shows: Array<any>;
@@ -19,21 +20,8 @@ export class AppComponent {
   }
 
   ngOnInit(): void {
-
-    this.hubConnection = new signalR.HubConnectionBuilder()
-                        .withUrl('http://channel-demo-app.centralus.cloudapp.azure.com/channelHub')
-                        .configureLogging(signalR.LogLevel.Information)
-                        .build();
-
-    this.hubConnection.start().catch(err => console.error(err.toString()));
-
-    this.hubConnection.on('getChannels', (channels: any) => {
-        this.zone.run(() => {
-          console.log("Length of received channels = " + channels.length);
-          this.channels = channels;        
-        });
-    });
-
+      this.createHubConnection(); 
+      this.listenForChannelUpdates();
   }
 
   getChannels(): void {
@@ -45,31 +33,53 @@ export class AppComponent {
 
   getShows(_channelID): void {
     if (this.hubConnection) {
-       console.log("Getting shows for channel: " + _channelID);
-       this.shows = [];
-       this.showsNotification = "";
-
-       this.hubConnection.stream("GetShows", _channelID)
-            .subscribe({
-                next: (show) => {
-                  console.log("Receiving a show: " + show.name);
-                  this.zone.run(() => {
-                    this.shows.push(show);
-                  });
-                },
-                complete: () => {
-                  this.zone.run(() => {
-                    console.log("Get shows operation completes");
-                    this.showsNotification = "Get shows operation completes";    
-                  });            
-                },
-                error: (err) => {
-                  this.zone.run(() => {
-                    console.log("An error happens in getting shows ...");
-                    this.showsNotification = "An error happens in getting shows ...";   
-                  });             
-                }
-            });
+        console.log("Getting shows for channel: " + _channelID);
+        this.shows = [];
+        this.showsNotification = "";
+        this.listenForShowUpdates(_channelID);
     }
-  }  
+  }
+
+  createHubConnection(): void {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+                       .withUrl(this.hubEndPoint)
+                       .configureLogging(signalR.LogLevel.Information)
+                       .build();
+
+    this.hubConnection.start().catch(err => console.error(err.toString()));
+  }
+
+  listenForChannelUpdates(): void {
+      this.hubConnection.on('getChannels', (channels: any) => {
+          this.zone.run(() => {
+              console.log("Length of received channels = " + channels.length);
+              this.channels = channels;        
+          });
+      });
+  }
+
+  listenForShowUpdates(_channelID): void {
+      let streamSubscriber = {
+          next: (show) => {
+              console.log("Receiving a show: " + show.name);
+              this.zone.run(() => {
+                  this.shows.push(show);
+              });
+          },
+          complete: () => {
+              this.zone.run(() => {
+                  console.log("Get shows operation completes");
+                  this.showsNotification = "Get shows operation completes";    
+              });            
+          },
+          error: (err) => {
+              this.zone.run(() => {
+                  console.log("An error happens in getting shows ...");
+                  this.showsNotification = "An error happens in getting shows ...";   
+              });             
+          }
+      }
+
+      this.hubConnection.stream("GetShows", _channelID).subscribe(streamSubscriber);
+  }
 }
